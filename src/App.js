1f98705e6aa5cba5094ef5329b8acef45a59563b
Frankluisravlo@gmail.com <Frankluisravlo@gmail.com>
2020-11-21 22:58:14 -0500

    installed bootstrap and added nav component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import Home from './components/home.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from './components/nav.js';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Home />
     </div>
   );
