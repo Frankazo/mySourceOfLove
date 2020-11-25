d3ec418753912dbde905438973ef16c840b7efea
Frankluisravlo@gmail.com <Frankluisravlo@gmail.com>
2020-11-24 23:19:41 -0500

    Added more css properties to handle mobile scenarios

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,30 +1,42 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 
+const Icon = styled.img`
+  width: 40px;
+  height: auto;
+`
+
+const MyNavbar = styled(Navbar)`
+  height: 75px;
+`
+
+
+
 const Footer = () => {
     return (
-    <Navbar fixed="bottom" bg="dark" variant="dark">
-      <Navbar.Brand href="#home" style={{fontFamily: `Dancing Script`, cursive: `true`, fontSize: `1.6em`}}>My Source of Love</Navbar.Brand>
+    <MyNavbar sticky="bottom" bg="dark" variant="dark">
+      <Navbar.Brand className="footer-logo" href="#home" style={{fontFamily: `Dancing Script`, cursive: `true`, fontSize: `1.6em`}}>My Source of Love</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
+        <Nav className="mr-auto footer-logo">
           <Nav.Link href="#copyrigth" disabled>All Rights Reserved</Nav.Link>
         </Nav>
         <Nav>
-        <Nav.Link href="https://www.instagram.com/mysourceoflove/" target="_blank" rel="noopener noreferrer"><img style={{ width: `40px`, height: `40px` }} src={require(`../images/instagram.svg`)} alt="Instagram" />
+        <Nav.Link href="https://www.instagram.com/mysourceoflove/" target="_blank" rel="noopener noreferrer"><Icon src={require(`../images/instagram.svg`)} alt="Instagram" />
                 </Nav.Link>
-                <Nav.Link href="https://www.facebook.com/mysourceoflove/" target="_blank" rel="noopener noreferrer"><img style={{ width: `40px`, height: `40px`, marginLeft: `15px` }} src={require(`../images/facebook.svg`)} alt="Facebook" />
+                <Nav.Link href="https://www.facebook.com/mysourceoflove/" target="_blank" rel="noopener noreferrer"><Icon src={require(`../images/facebook.svg`)} alt="Facebook" />
                 </Nav.Link>
-                <Nav.Link href="https://twitter.com/hellyydaya" target="_blank" rel="noopener noreferrer"><img style={{ width: `40px`, height: `40px`, marginLeft: `15px` }} src={require(`../images/twitter.svg`)} alt="Twitter" />
+                <Nav.Link href="https://twitter.com/hellyydaya" target="_blank" rel="noopener noreferrer"><Icon src={require(`../images/twitter.svg`)} alt="Twitter" />
                 </Nav.Link>
-                <Nav.Link href="https://www.youtube.com/channel/UCQK5tCDg6zGWBsmaEn1Gu7Q" target="_blank" rel="noopener noreferrer"><img style={{ width: `40px`, height: `40px`, marginLeft: `15px` }} src={require(`../images/youtube.svg`)} alt="Youtube" />
+                <Nav.Link href="https://www.youtube.com/channel/UCQK5tCDg6zGWBsmaEn1Gu7Q" target="_blank" rel="noopener noreferrer"><Icon src={require(`../images/youtube.svg`)} alt="Youtube" />
                 </Nav.Link>
-                <Nav.Link href="mailto: HellyDayana@sourceoflove.com"><img style={{ width: `40px`, height: `40px`, marginLeft: `15px` }} src={require(`../images/envelope.svg`)} alt="Email" /></Nav.Link>
+                <Nav.Link href="mailto: HellyDayana@sourceoflove.com"><Icon src={require(`../images/envelope.svg`)} alt="Email" /></Nav.Link>
             </Nav>
       </Navbar.Collapse>
-    </Navbar>
+    </MyNavbar>
     )
 }
 
